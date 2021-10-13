405ceaea48f391a67f703d43e62c9a5c8b8831bc
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-10-13 20:00:11 +0530

    repo redirect link added

--- random-concepts-neog/src/components/Footer.js
+++ random-concepts-neog/src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
             <p style={{fontSize:"2rem"}}> About </p>
             <p className="ftr-para"> Level Zero had  plenty of minor and major concepts and apparently it is quite hard to remember all of those for eternity,<span style={{fontWeight:"bold"}}> concepts cards </span> assists  you to revise those concepts by randomly presenting you a variant of  level zero question and its answer, you can use this app whenever you're short on time, busy, tired or in  a boring  family function.  </p>
             <p className="ftr-para">Note:-This app is made solely for revision only and hence is not supposed to be used as a replacement of your full-fledged interview preparation.</p>
-            <p  className="ftr-para">Head over to  my <a href="#">GitHub Repo <span><i className="ion-social-github-outline"></i></span> </a> if you're interested in contributing more questions 
+            <p  className="ftr-para">Head over to  my <a href="https://github.com/Sen-442b/level-zero-concepts-cards/tree/master/random-concepts-neog">GitHub Repo <span><i className="ion-social-github-outline"></i></span> </a> if you're interested in contributing more questions 
 </p>
 
         </div>
